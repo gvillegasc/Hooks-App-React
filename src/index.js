5d5cccd725933175b9429da3c89b905bf2b782ec
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-07-12 22:18:39 -0500

    Usando funcion memo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import ReactDOM from 'react-dom';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHook } from './components/03-examples/MultipleCustomHook';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Memorize } from './components/06-memos/Memorize';
 
-ReactDOM.render(<RealExampleRef />, document.getElementById('root'));
+ReactDOM.render(<Memorize />, document.getElementById('root'));
