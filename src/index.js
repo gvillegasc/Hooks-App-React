00bf2695b49bbe70477eae4b5add01c06d75b291
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-07-12 21:45:05 -0500

    Añadiendo useFech

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-import { MultipleCustomHook } from './components/03-examples/MultipleCustomHook';
+// import { MultipleCustomHook } from './components/03-examples/MultipleCustomHook';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
-ReactDOM.render(<MultipleCustomHook />, document.getElementById('root'));
+ReactDOM.render(<RealExampleRef />, document.getElementById('root'));
