1639b15e9c4cdd65a876fac5533793d8ca2210b7
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-07-19 19:18:51 -0500

    Guardando tareas en localstorage

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,24 +1,33 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { todoReducer } from './todoReducer';
+import { useForm } from '../../hooks/useForm';
 import './styles.css';
 
-const initialState = [
-	{
-		id: new Date().getTime(),
-		desc: 'Aprender React',
-		done: false,
-	},
-];
+const init = () => {
+	return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
 export const TodoApp = () => {
-	const [todos, dispatch] = useReducer(todoReducer, initialState);
-	console.log(todos);
+	const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+	const [{ description }, handleInputChange, reset] = useForm({
+		description: '',
+	});
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
+		if (description.trim().length <= 1) {
+			return;
+		}
+
 		const newTodo = {
 			id: new Date().getTime(),
-			desc: 'Nueva tarea',
+			desc: description,
 			done: false,
 		};
 
@@ -28,6 +37,8 @@ export const TodoApp = () => {
 		};
 
 		dispatch(action);
+
+		reset();
 	};
 	return (
 		<div>
@@ -57,6 +68,8 @@ export const TodoApp = () => {
 							className="form-control"
 							placeholder="Aprender..."
 							autoComplete="off"
+							value={description}
+							onChange={handleInputChange}
 						></input>
 						<button
 							type="submit"
