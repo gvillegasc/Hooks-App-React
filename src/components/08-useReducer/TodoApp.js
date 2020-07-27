5065e0eb6fabfb840366314ca8189ee6f04a4664
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-07-26 20:20:06 -0500

    Añadiendo reducer para completar tareas

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -18,6 +18,24 @@ export const TodoApp = () => {
 		localStorage.setItem('todos', JSON.stringify(todos));
 	}, [todos]);
 
+	const handleDelete = (todoId) => {
+		console.log(todoId);
+
+		const action = {
+			type: 'delete',
+			payload: todoId,
+		};
+
+		dispatch(action);
+	};
+
+	const handleToggle = (todoId) => {
+		dispatch({
+			type: 'toggle',
+			payload: todoId,
+		});
+	};
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
@@ -50,10 +68,19 @@ export const TodoApp = () => {
 					<ul className="list-group list-group-flush">
 						{todos.map((todo, i) => (
 							<li key={todo.id} className="list-group-item">
-								<p className="text-center">
+								<p
+									// className={todo.done ? 'complete' : ''}
+									className={`${todo.done && 'complete'}`}
+									onClick={() => handleToggle(todo.id)}
+								>
 									{i + 1}. {todo.desc}
 								</p>
-								<button className="btn btn-danger">Borrar</button>
+								<button
+									className="btn btn-danger"
+									onClick={() => handleDelete(todo.id)}
+								>
+									Borrar
+								</button>
 							</li>
 						))}
 					</ul>
